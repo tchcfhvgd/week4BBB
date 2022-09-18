74ab8cded6671a71cc10fda8ea77ee595ec6f81f
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2022-09-18 02:02:45 -0400

    die

--- source/NGio.hx
+++ source/NGio.hx
@@ -58,6 +58,7 @@ class NGio
 		}
 	}
 
+/*
 	public function new(api:String, encKey:String, ?sessionId:String)
 	{
 		trace("connecting to newgrounds");
@@ -74,18 +75,19 @@ class NGio
 		{
 			/* a session_id was found in the loadervars, this means the user is playing on newgrounds.com
 			 * and we should login shortly. lets wait for that to happen
-			 */
+			 
 			trace("attempting login");
 			NG.core.onLogin.add(onNGLogin);
 		}
 		else
 		{
-			/* They are NOT playing on newgrounds.com, no session id was found. We must start one manually, if we want to.
+			 They are NOT playing on newgrounds.com, no session id was found. We must start one manually, if we want to.
 			 * Note: This will cause a new browser window to pop up where they can log in to newgrounds
-			 */
+			 
 			NG.core.requestLogin(onNGLogin);
 		}
 	}
+
 
 	function onNGLogin():Void
 	{
@@ -101,6 +103,7 @@ class NGio
 
 		ngDataLoaded.dispatch();
 	}
+*/
 
 	// --- MEDALS
 	function onNGMedalFetch():Void
