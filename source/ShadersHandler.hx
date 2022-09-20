3ec1035449eb92fa429276e88e10479be1522c9f
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-09-21 00:46:20 +0300

    Update ShadersHandler.hx

--- source/ShadersHandler.hx
+++ source/ShadersHandler.hx
@@ -11,19 +11,19 @@ class ShadersHandler
 
 	public static function setChrome(chromeOffset:Float):Void
 	{
-		chromaticAberration.shader.data.rOffset.value = [chromeOffset];
-		chromaticAberration.shader.data.gOffset.value = [0.0];
-		chromaticAberration.shader.data.bOffset.value = [chromeOffset * -1];
+		chromaticAberration.shader.rOffset.value = [chromeOffset];
+		chromaticAberration.shader.gOffset.value = [0.0];
+		chromaticAberration.shader.bOffset.value = [chromeOffset * -1];
 	}
 	public static function setRadialBlur(x:Float=640,y:Float=360,power:Float=0.03):Void
 	{
-		radialBlur.shader.data.blurWidth.value = [power];
-		radialBlur.shader.data.cx.value = [x/2560];
-		radialBlur.shader.data.cy.value = [y/1440];
+		radialBlur.shader.blurWidth.value = [power];
+		radialBlur.shader.cx.value = [x/2560];
+		radialBlur.shader.cy.value = [y/1440];
 	}
 	public static function setBlur(angle:Float,power:Float=0.1):Void
 	{
-		radialBlur.shader.data.angle.value = [angle];
-		radialBlur.shader.data.strength.value = [power];
+		radialBlur.shader.angle.value = [angle];
+		radialBlur.shader.strength.value = [power];
 	}
 }
